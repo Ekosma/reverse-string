be1834e1906d0ed190a2c76787d3c748d5fe95bc
Evelyn <evelyn.a.kosma@gmail.com>
2022-05-21 09:38:45 -0500

    ruby string reverse functional, all tests passing

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,5 +1,5 @@
 def reverse_string(str)
-  # type your code in here
+  return str.reverse
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -16,3 +16,5 @@ end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+# just use the reverse method, compared to javascript where 
+# you have to convert it to an array first via split, then join. 
